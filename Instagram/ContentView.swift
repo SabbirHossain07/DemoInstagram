8574be7c6f0579b89caf097f79f8ce5b948a7cb3
SabbirHossain07 <sabbirsopnil07@gmail.com>
2023-06-01 22:14:43 +0600

    Initial Commit

---
+++ Instagram/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Instagram
+//
+//  Created by Sopnil Sohan on 1/6/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Instagram/InstagramApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramApp.swift
+//  Instagram
+//
+//  Created by Sopnil Sohan on 1/6/23.
+//
+
+import SwiftUI
+
+@main
+struct InstagramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
